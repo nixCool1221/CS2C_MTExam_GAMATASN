0e97cac0f165dcb3cf5a2f7c0197fb85a123a611
Gamata Sophia Nicole M. <mecossophia@gmail.com>
2024-10-31 11:21:02 -0400

    Add files via upload

---
+++ studentEnrollment.js
@@ -0,0 +1,20 @@
+// Initialize arrays for subjects
+var DSA = [];
+var PL = [];
+var Networks = [];
+
+// Function to display subjects
+function displaySubjects() {
+    console.log("Select a subject:");
+    console.log("(A) DSA");
+    console.log("(B) PL");
+    console.log("(C) Networks");
+}
+
+// Function to display operations
+function displayOperations() {
+    console.log("Select an operation:");
+    console.log("(A) Enroll");
+    console.log("(B) Unenroll");
+    console.log("(C) Select Another Subject");
+}
